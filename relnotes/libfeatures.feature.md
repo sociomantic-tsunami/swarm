8cf45ab6ab5604a17770ccf1055cbdf1cd919fe6
Gavin Norman <gavin.norman@sociomantic.com>
2017-07-11 15:09:36 +0200

    Add LibFeatures module
    Meta module defining constants that match swarm feature releases
    allowing downstream libraries to conditionally implement
    functionality relying on later swarm versions.

---
+++ relnotes/libfeatures.feature.md
@@ -0,0 +1,6 @@
+* `swarm.LibFeatures`
+
+  Added a meta module defining constants that match swarm feature releases
+  allowing downstream libraries to conditionally implement functionality relying
+  on later swarm versions.
+
---
+++ src/swarm/LibFeatures.d
@@ -0,0 +1,23 @@
+/*******************************************************************************
+
+    Meta module defining constants that match swarm feature releases allowing
+    downstream libraries to conditionally implement functionality relying on
+    later swarm versions.
+
+    Copyright:
+        Copyright (c) 2017 sociomantic labs GmbH. All rights reserved
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module swarm.LibFeatures;
+
+const has_features_4_2 = true;
+const has_features_4_1 = true;
+const has_features_4_0 = true;
+const has_features_3_3 = true;
+const has_features_3_2 = true;
+const has_features_3_1 = true;
+const has_features_3_0 = true;
