1ac28db550794c3f060192111d25250420d877f6
davideckardt <david.eckardt@sociomantic.com>
2017-07-11 17:12:16 +0200

    Add a way of calling MessageSender.flush() for a request handler
    When TCP Cork is enabled, it will likely hold the small control messages
    in a buffer for 200ms. This delay is not acceptable for Suspend, Stop or
    similar messages that need a fast response, so a request handler needs
    EventDispatcher.flush(), which calls MessageSender.flush().

--- src/swarm/neo/connection/ConnectionBase.d
+++ src/swarm/neo/connection/ConnectionBase.d
@@ -109,6 +109,16 @@ abstract class ConnectionBase: ISelectClient
 
         /***********************************************************************
 
+            If this flag is true and `sendRequestPayload` is waiting for
+            `EPOLLOUT` then it will call `sender.flush` after it has sent the
+            remaining data.
+
+        ***********************************************************************/
+
+        public bool flush_requested;
+
+        /***********************************************************************
+
             The queue of ids of requests waiting to send a message.
 
         ***********************************************************************/
@@ -357,6 +367,8 @@ abstract class ConnectionBase: ISelectClient
         {
             bool finish_sending = false;
 
+            scope (exit) this.flush_requested = false;
+
             this.outer.getPayloadForSending(
                 id,
                 ( in void[][] payload )
@@ -373,6 +385,8 @@ abstract class ConnectionBase: ISelectClient
                 // Register the socket for both input and output
                 this.outer.registerEpoll(true);
                 this.outer.sender.finishSending(this.suspend());
+                if (this.flush_requested)
+                    this.outer.sender.flush();
             }
         }
     }
@@ -1031,6 +1045,31 @@ abstract class ConnectionBase: ISelectClient
 
     abstract protected void getPayloadForSending ( RequestId id,
         void delegate ( in void[][] payload ) send );
+
+    /***************************************************************************
+
+        Requests to send outgoing data buffered by the OS for the socket
+        immediately. By default outgoing data are buffered to the maximum
+        payload of a TCP frame using the Linux TCP_CORK socket option.
+
+        This method is intended to be called right after `getPayloadForSending`
+        has called the `send` delegate.
+
+    ***************************************************************************/
+
+    public void flush ( )
+    {
+        // Flush the OS-maintained (TCP_CORK) socket output buffer. This will
+        // immediately send all data that have been accepted by `sendmsg(2)`.
+        this.sender.flush();
+        // `this.sender` may in addition buffer data that have not been accepted
+        // by `sendmsg` - that is, `send(2)` returned a value less than the
+        // buffer length argument - and wait for `EPOLLOUT` on the socket to
+        // call `sendmsg` again  with the remaining data. Set a flag to flush
+        // the OS-maintained (TCP_CORK) socket output buffer when `sendmsg`
+        // eventually accepted all outstanding data.
+        this.send_loop.flush_requested = true;
+    }
 
     /***************************************************************************
 
