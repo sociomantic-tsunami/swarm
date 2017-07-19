b418ef822a22323d1430caa36af034e1c8b836fa
Gavin Norman <gavin.norman@sociomantic.com>
2017-07-19 11:55:16 +0200

    Move node-specific credentials code into own module
    The credentials file parsing code will also be needed in the client.
    
    Part of #53.

---
+++ src/swarm/neo/authentication/NodeCredentials.d
@@ -0,0 +1,114 @@
+/*******************************************************************************
+
+    Node credentials
+
+    copyright: Copyright (c) 2017 sociomantic labs GmbH. All rights reserved
+
+    License:
+        Boost Software License Version 1.0. See LICENSE_BOOST.txt for details.
+
+*******************************************************************************/
+
+module swarm.neo.authentication.NodeCredentials;
+
+/// ditto
+public class Credentials
+{
+    import swarm.neo.authentication.Credentials;
+    import CredFile = swarm.neo.authentication.CredentialsFile;
+    import HmacDef = swarm.neo.authentication.HmacDef;
+
+    import ocean.transition;
+    import ocean.io.device.File;
+    import ocean.net.util.QueryParams;
+    import ocean.text.convert.Format;
+
+    /***************************************************************************
+
+        The credentials registry. A pointer to it can be obtained by
+        `credentials()`, it changes when `update()` is called.
+
+    ***************************************************************************/
+
+    private HmacDef.Key[istring] credentials_;
+
+    /***************************************************************************
+
+        The credentials file path.
+
+    ***************************************************************************/
+
+    private istring filepath;
+
+    /***************************************************************************
+
+        Constructor, reads the credentials from the file (by calling
+        `update()`).
+
+        Params:
+            filepath = the credentials file path
+
+        Throws:
+            See `update()`.
+
+    ***************************************************************************/
+
+    public this ( istring filepath )
+    {
+        this.filepath = filepath;
+        this.update();
+    }
+
+    /***************************************************************************
+
+        Obtains a pointer to the credentials, which change if `update` is
+        called.
+
+        Returns:
+            a pointer to the credentials.
+
+    ***************************************************************************/
+
+    public Const!(HmacDef.Key[istring])* credentials ( )
+    {
+        return &this.credentials_;
+    }
+
+    /***************************************************************************
+
+        Updates the credentials from the file, and changes this instance to
+        refer to the updated credentials on success. On error this instance will
+        keep referring to the same credentials.
+
+        Throws:
+            - IOException on file I/O error.
+            - ParseException on invalid file size or content; that is, if
+              - the file size is greater than Credentials.LengthLimit.File,
+              - a name
+                - is empty (zero length),
+                - is longer than Credentials.LengthLimit.File,
+                - contains non-graph characters,
+              - a key
+                - is empty (zero length),
+                - is longer than Credentials.LengthLimit.File * 2,
+                - has an odd (not even) length,
+                - contains non-hexadecimal digits.
+
+    ***************************************************************************/
+
+    public void update ( )
+    {
+        this.credentials_ = CredFile.parse(this.filepath);
+    }
+
+    /***************************************************************************
+
+        Specialised exception class containing the file and line where a parsing
+        error occurred.
+
+    ***************************************************************************/
+
+    deprecated("Refer to CredentialsParseException in "
+        "swarm.neo.authentication.CredentialsFile directly")
+    public alias CredFile.CredentialsParseException ParseException;
+}
