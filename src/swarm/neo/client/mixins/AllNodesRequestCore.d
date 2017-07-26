sociomantic-tsunami/swarm

commit a005c1d3d2f3bb248af229502c5d8cebfa9ab219

Retry AllNodesRequest on EpollException, not just on IOError
IOError are representing just a subset of the read/write issues that
can happen on the connection. For example, hangup of the remote party
will result in the IOWarning, or being unable to send any packet to the
remote party will result in (more general) EpollException. In all these
reasons, where the disconnection occurs, request should try to reconnect
to the node and continue request, not just on IOErrors.

src/swarm/neo/client/mixins/AllNodesRequestCore.d
@@ -96,7 +96,7 @@ public struct AllNodesRequest ( Request, Connector, Disconnected, Initialiser,
     Handler )
 {
     import swarm.neo.client.RequestOnConn;
-    import ocean.io.select.protocol.generic.ErrnoIOException : IOError;
+    import ocean.io.select.selector.EpollException;
 
     /// Policy object for handling connection establishment.
     private Connector connector;
@@ -142,7 +142,7 @@ public struct AllNodesRequest ( Request, Connector, Disconnected, Initialiser,
             }
             // Only retry in the case of a connection error. Other errors
             // indicate internal problems and should not be retried.
-            catch ( IOError e )
+            catch ( EpollException e )
             {
                 this.disconnected(e);
                 reconnect = true;

commit 02905158371ce6bca21f3bbc3f353db947de97fa

Also retry all-nodes requests on I/O error
a005c1d3d2f3bb248af229502c5d8cebfa9ab219 inadvertently altered the
reconnection behaviour of all-nodes requests by only reconnecting after an
error event in epoll. We (obviously) also need to reconnect after a normal
connection I/O error (e.g. broken pipe).

src/swarm/neo/client/mixins/AllNodesRequestCore.d
@@ -97,6 +97,7 @@ public struct AllNodesRequest ( Request, Connector, Disconnected, Initialiser,
 {
     import swarm.neo.client.RequestOnConn;
     import ocean.io.select.selector.EpollException;
+    import ocean.io.select.protocol.generic.ErrnoIOException;
 
     /// Policy object for handling connection establishment.
     private Connector connector;
@@ -140,8 +141,14 @@ public struct AllNodesRequest ( Request, Connector, Disconnected, Initialiser,
 
                 this.handler();
             }
-            // Only retry in the case of a connection error. Other errors
-            // indicate internal problems and should not be retried.
+            // Only retry in the case of a connection I/O error or error event
+            // in epoll. Other errors indicate internal problems and should not
+            // be retried.
+            catch ( IOError e )
+            {
+                this.disconnected(e);
+                reconnect = true;
+            }
             catch ( EpollException e )
             {
                 this.disconnected(e);