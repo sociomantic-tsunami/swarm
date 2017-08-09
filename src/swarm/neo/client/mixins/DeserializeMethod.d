6df5e1c7ea080a8afa5b44a25ea7bbaa43398252
Gavin Norman <gavin.norman@sociomantic.com>
2017-08-09 15:38:48 +0200

    Add DeserializeMethods helper method mixin
    Fixes #105.

---
+++ src/swarm/neo/client/mixins/DeserializeMethod.d
@@ -0,0 +1,76 @@
+/*******************************************************************************
+
+    Mixin for method to deserialize a received record.
+
+    Copyright:
+        Copyright (c) 2017 sociomantic labs GmbH. All rights reserved
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module swarm.neo.client.mixins.DeserializeMethod;
+
+import ocean.util.serialize.contiguous.MultiVersionDecorator;
+
+/*******************************************************************************
+
+    Mixin for method to deserialize a received record.
+
+    Params:
+        src = the buffer to be deserialized
+
+*******************************************************************************/
+
+template DeserializeMethod ( alias src )
+{
+    import ocean.util.serialize.contiguous.Contiguous;
+    import ocean.util.serialize.Version;
+    import ocean.util.serialize.contiguous.Deserializer;
+    import ocean.util.serialize.contiguous.MultiVersionDecorator;
+
+    /***************************************************************************
+
+        Deserializes `src` into `dst`, using the provided version decorater, if
+        required.
+
+        Params:
+            T = type of struct to deserialize
+            dst = deserialization destination
+
+        Returns:
+            pointer to deserialized struct (points to the buffer inside dst)
+
+    ***************************************************************************/
+
+    public T* deserialize ( T ) ( ref Contiguous!(T) dst )
+    {
+        static if ( Version.Info!(T).exists )
+        {
+            if ( client_deserializer_version_decorator is null )
+                client_deserializer_version_decorator = new VersionDecorator;
+
+            return client_deserializer_version_decorator
+                .loadCopy!(T)(src, dst).ptr;
+        }
+        else
+        {
+            return Deserializer.deserialize(src, dst).ptr;
+        }
+    }
+}
+
+/*******************************************************************************
+
+    VersionDecorator singleton used by all mixed-in deserialize() methods when
+    handling versioned structs.
+
+    Must be public as it needs to be accessible from the point where the
+    deserialize method is mixed in. Not intended to be used externally by users.
+    The object is given an unusually long name so as to reduce the likelihood of
+    name clashes with other symbols.
+
+*******************************************************************************/
+
+public VersionDecorator client_deserializer_version_decorator;
