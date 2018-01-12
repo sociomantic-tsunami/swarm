a8fdb3325f6afa3fbb25304e70598d5bef05a303
Gavin Norman <gavin.norman@sociomantic.com>
2018-01-12 15:08:54 +0100

    Convert args of test client notifiers to be const
    Fixes #7. (This is actually the only relevant part of this issue in swarm;
    it applies more to the protocol repos.)

--- integrationtest/neo/client/request/Get.d
+++ integrationtest/neo/client/request/Get.d
@@ -68,4 +68,4 @@ public alias SmartUnion!(NotificationUnion) Notification;
 
 *******************************************************************************/
 
-public alias void delegate ( Notification, Args ) Notifier;
+public alias void delegate ( Notification, Const!(Args) ) Notifier;
--- integrationtest/neo/client/request/GetAll.d
+++ integrationtest/neo/client/request/GetAll.d
@@ -88,7 +88,7 @@ public alias SmartUnion!(NotificationUnion) Notification;
 
 *******************************************************************************/
 
-public alias void delegate ( Notification, Args ) Notifier;
+public alias void delegate ( Notification, Const!(Args) ) Notifier;
 
 /*******************************************************************************
 
