beb77931a36a0e94f82254d3a6be128bd2a12cb3
Gavin Norman <gavin.norman@sociomantic.com>
2018-03-01 16:26:24 +0100

    Change `EventDispatcher.shutdownWithProtocolError` to throw, not return
    The returned exception is intended to always be thrown, and it would be a
    bug, really, if the caller did not do so. Therefore, making the method
    throw directly simplifies usage and removes one vector for bugs.

--- src/swarm/neo/node/ConnectionHandler.d
+++ src/swarm/neo/node/ConnectionHandler.d
@@ -481,7 +481,7 @@ class ConnectionHandler : IConnectionHandler
         }
         else
         {
-            throw connection.event_dispatcher.shutdownWithProtocolError(
+            connection.event_dispatcher.shutdownWithProtocolError(
                 "First request message contains no command"
             );
         }
