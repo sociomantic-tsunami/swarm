47f4bd48a50c98700e4a76bf640052235122b00f
Nemanja Boric <nemanja.boric@sociomantic.com>
2018-07-16 16:24:07 +0000

    Auto-convert to D2

--- src/swarm/neo/AddrPort.d
+++ src/swarm/neo/AddrPort.d
@@ -54,7 +54,7 @@ public struct AddrPort
 
     public bool is_set ( ) // const
     {
-        return this.naddress && this.nport;
+        return (&this).naddress && (&this).nport;
     }
 
     /***************************************************************************
@@ -67,7 +67,7 @@ public struct AddrPort
 
     public uint address ( )
     {
-        return ntohl(this.naddress);
+        return ntohl((&this).naddress);
     }
 
     /***************************************************************************
@@ -85,7 +85,7 @@ public struct AddrPort
 
     public uint address ( uint address )
     {
-        this.naddress = htonl(address);
+        (&this).naddress = htonl(address);
         return address;
     }
 
@@ -119,7 +119,7 @@ public struct AddrPort
 
         if (inet_aton(buf.ptr, &result))
         {
-            this.naddress = result.s_addr;
+            (&this).naddress = result.s_addr;
             return true;
         }
 
@@ -155,7 +155,7 @@ public struct AddrPort
     {
         verify(dst.length >= INET_ADDRSTRLEN,
             "dst.length expected to be at least INET_ADDRSTRLEN");
-        auto src = in_addr(this.naddress);
+        auto src = in_addr((&this).naddress);
         inet_ntop(AF_INET, &src, dst.ptr, cast(socklen_t)dst.length);
         // The only possible errors for inet_ntop  are a wrong address family
         // and `dst.length < INET_ADDRSTRLEN`. Neither can be the case here.
@@ -164,7 +164,7 @@ public struct AddrPort
 
     unittest
     {
-        typeof(*this) x;
+        typeof(*(&this)) x;
         test(x.setAddress("192.168.222.111"));
         test!("==")(x.address_bytes, [cast(ubyte)192, 168, 222, 111]);
 
@@ -185,7 +185,7 @@ public struct AddrPort
 
     public ushort port ( ) // const
     {
-        return ntohs(this.nport);
+        return ntohs((&this).nport);
     }
 
     /***************************************************************************
@@ -202,7 +202,7 @@ public struct AddrPort
 
     public ushort port ( ushort port )
     {
-        return this.nport = htons(port);
+        return (&this).nport = htons(port);
     }
 
     /***************************************************************************
@@ -229,7 +229,7 @@ public struct AddrPort
 
     public ubyte[] address_bytes ( )
     {
-        return (cast(ubyte*)&this.naddress)[0 .. this.naddress.sizeof];
+        return (cast(ubyte*)&(&this).naddress)[0 .. (&this).naddress.sizeof];
     }
 
      /***************************************************************************
@@ -246,8 +246,8 @@ public struct AddrPort
     {
         sockaddr_in result;
         result.sin_family      = AF_INET;
-        result.sin_port        = this.nport;
-        result.sin_addr.s_addr = this.naddress;
+        result.sin_port        = (&this).nport;
+        result.sin_addr.s_addr = (&this).naddress;
         return result;
     }
 
@@ -263,11 +263,11 @@ public struct AddrPort
 
     ***************************************************************************/
 
-    public typeof(this) set ( sockaddr_in src )
+    public typeof((&this)) set ( sockaddr_in src )
     {
-        this.naddress = src.sin_addr.s_addr;
-        this.nport    = src.sin_port;
-        return this;
+        (&this).naddress = src.sin_addr.s_addr;
+        (&this).nport    = src.sin_port;
+        return (&this);
     }
 
     /***************************************************************************
@@ -294,7 +294,7 @@ public struct AddrPort
          * Make sure address & port really fit in a long without a signed
          * overflow.
          */
-        static assert(this.address.sizeof + this.port.sizeof < long.sizeof);
-        return ((cast(long)this.address) << (this.port.sizeof * 8)) | this.port;
+        static assert((&this).address.sizeof + (&this).port.sizeof < long.sizeof);
+        return ((cast(long)(&this).address) << ((&this).port.sizeof * 8)) | (&this).port;
     }
 }
