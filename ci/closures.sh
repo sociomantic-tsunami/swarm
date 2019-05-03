144748a676345a40bc5b04037f77a587d693fa3f
Tiyash Basu <tiyash.basu@dunnhumby.com>
2019-05-03 16:07:15 +0200

    Update build directives to be D2-only

--- ci/closures.sh
+++ ci/closures.sh
@@ -4,9 +4,6 @@ set -xe
 # Enables printing of all potential GC allocation sources to stdout
 export DFLAGS=-vgc
 
-# Prepare sources
-beaver dlang make d2conv
-
 # Ensure that there are no lines about closure allocations in the output
 # Filters away errors from submodules to focus on swarm own code only
 ! beaver dlang make fasttest 2>&1 | grep "\./src/.*\<closure\>"
