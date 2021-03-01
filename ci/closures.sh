fd6023c1cfac37489758fd87c6a27c6f26a46b92
Geod24 <pro.mathias.lang@gmail.com>
2021-03-01 11:20:40 +0900

    Update all dependencies, drop Travis and beaver, use Github actions
    As was done in Ocean and Turtle already, we drop support for DMD < 2092, remove beaver,
    and Travis (which has gone EOL), instead relying on a Github-only CI.

--- ci/closures.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -xe
-
-# Enables printing of all potential GC allocation sources to stdout
-export DFLAGS=-vgc
-
-# Ensure that there are no lines about closure allocations in the output
-# Filters away errors from submodules to focus on swarm own code only
-! beaver dlang make fasttest 2>&1 | grep "\./src/.*\<closure\>"
